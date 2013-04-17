4aa51a021f7bd3b68ad3811328e471eac2b4b51f
Omar Rayward <orayward@yahoo.com>
2013-04-17 10:05:59 -0700

    Another way of solving the same problem with Regexp and backtracking

--- justify_string.rb
+++ justify_string.rb
@@ -10,13 +10,35 @@ def justify(string, num)
   i = 0
   extra.times {list[0]+= ' '; i+=1}
   result = list.join('').rstrip
-  puts result.size
-  return result.inspect
+  # puts result.size
+  return result
 end
 
+puts justify('aa bb cc', 20)
 
 
+# This way of solving this problem was envisioned by Ryan Moser.
+# The lessons to be learned by the way he solved this problem are:
+# 1. Creation of Regular Expressions with String Interpolation
+# 2. How to use backtracking in the second argument of a gsub or sub
+#    when a regex is used as the first argumet of the same function
+# 3. sub and gsub return nil if the obj (<str>) doesn't match the first
+#    argument in the sub or gsub
 
-a= 'a a aa a bb'
-puts justify(a, 50)
+def justify(str, width)
+  whitespace = ' '
+  while str.length < width
+    spacing = Regexp.new("(\\w)#{whitespace}(\\w)")
+    sub = str.sub!(spacing, "\\1 #{whitespace}\\2")
+    whitespace << ' ' unless sub
+  end
+  # puts str.size
+  str
+end
+
+puts justify('aa bb cc', 20)
+# puts justify('aa b cc', 70)
+# puts justify('asdfga bd c', 50)
+
+
 
