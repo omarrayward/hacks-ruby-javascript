0cc7ba7cfb85a19022f2025ac3785320d4dcd2e8
Omar Rayward <orayward@yahoo.com>
2013-04-21 12:10:28 -0700

    A better implementation by Jesse Farmer is added to the code

--- parens.rb
+++ parens.rb
@@ -1,6 +1,6 @@
 # Create a method that returns true if the open and close parenthesis match in a proper way
 require 'benchmark'
-def match? string
+def match_omar? string
   counter = 0 # we start the counter on zero. Every "(" will increase counter by 1, and every ")" will decrease couner by 1.
   string.each_char do |char|
     counter += 1 if char == "("
@@ -18,7 +18,7 @@ end
 # puts match?("(hello))")                 == false
 # puts match?("(hello, how are(you))")    == true
 
-def match2? string
+def match_zee? string
   translation = { "(" => 1, ")" => -1 }
   string.each_char.inject(0) do |counter, char|
     return false if counter == -1
@@ -26,12 +26,29 @@ def match2? string
   end == 0
 end
 
+def match_jesse?(string)
+  string.each_char.inject(0) do |counter, char|
+    return false if counter < 0
+    case char
+    when '('
+      counter + 1
+    when ')'
+      counter - 1
+    else
+      counter
+    end
+  end.zero?
+end
+
+
 long_ass_matched_string = ("("*1000) + (")"*1000)
 long_ass_unmatched_string = ("("*1000) + (")"*1001)
 
 Benchmark.bmbm do |x|
-  x.report('omar'){10000.times{match?(long_ass_unmatched_string)}}
-  x.report('zee'){10000.times{match2?(long_ass_unmatched_string)}}
-  x.report('omar'){10000.times{match?(long_ass_matched_string)}}
-  x.report('zee'){10000.times{match?(long_ass_matched_string)}}
+  x.report('omar'){10000.times{match_omar?(long_ass_unmatched_string)}}
+  x.report('zee'){10000.times{match_zee?(long_ass_unmatched_string)}}
+  x.report('jesee'){10000.times{match_jesse?(long_ass_unmatched_string)}}
+  x.report('omar'){10000.times{match_omar?(long_ass_matched_string)}}
+  x.report('zee'){10000.times{match_zee?(long_ass_matched_string)}}
+  x.report('jesee'){10000.times{match_jesse?(long_ass_matched_string)}}
 end
