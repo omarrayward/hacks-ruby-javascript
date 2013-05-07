706a4e98aa2e7b2d899b55253f956502828ffd07
Omar Rayward <orayward@yahoo.com>
2013-05-06 22:41:22 -0700

    Implement map with each, inject with each and map with inject

---
+++ map_inject.rb
@@ -0,0 +1,34 @@
+# 3 Challenges in 1:
+# * Implement map using only each
+# * Implement inject using only each
+# * Implement map using inject
+
+class Array
+  def map!
+    new_array = []
+    self.each do |element|
+      new_array << yield(element)
+    end
+    new_array
+  end
+
+  def inject(memo= self[0])
+    self.each do |element|
+      memo = yield(memo, element)
+    end
+    memo
+  end
+
+  def map2!
+    memo = []
+    self.inject(memo) do |memo, element|
+      memo << yield(element)
+    end
+    memo
+  end
+
+end
+
+p [1,2,3].map!{|e| e*2}
+p [1,2,3].map2!{|e| e*2}
+p [1,2,3].inject{|memo, e| memo += e}
